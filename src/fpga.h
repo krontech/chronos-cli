141261aec962c6419c6cdb7d629d3d42087e1a97
Owen Kirby <oskirby@gmail.com>
2017-10-24 16:41:04 -0700

    Add GPLv3 license and copyright headers.

--- src/fpga.h
+++ src/fpga.h
@@ -1,6 +1,21 @@
-/* Copyright 2017 Kron Technologies. All Rights Reserved. */
-#ifndef _GPMC_H
-#define _GPMC_H
+/****************************************************************************
+ *  Copyright (C) 2017 Kron Technologies Inc <http://www.krontech.ca>.      *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ****************************************************************************/
+#ifndef _FPGA_H
+#define _FPGA_H
 
 #include <stdint.h>
 #include <stdio.h>
@@ -321,4 +336,4 @@ int fpga_load(const char *spi, const char *bitstream, FILE *log);
 uint16_t fpga_sci_read(struct fpga *fpga, uint8_t addr);
 void fpga_sci_write(struct fpga *fpga, uint8_t addr, uint16_t value);
 
-#endif /* _GPMC_H */
+#endif /* _FPGA_H */
