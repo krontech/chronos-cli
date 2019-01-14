a9cf42c67b8fb169d3cade38323405f5af9f2037
Owen Kirby <oskirby@gmail.com>
2019-01-14 14:47:41 -0800

    Add Debian packaging of python3-pychronos module.
    The pychronos module provides a Python interface to the Chronos FPGA
    and image sensor registers.

--- src/pychronos/setup.py
+++ src/pychronos/setup.py
@@ -2,13 +2,13 @@
 from distutils.core import setup, Extension
 
 pychronos = Extension('pychronos',
-                      include_dirs = ['lib'],
-                      depends = ['pychronos/pychronos.h',
-                                 'lib/fpga.h',
-                                 'lib/lux1310.h'],
-                      sources = ['pychronos/module.c',
-                                 'pychronos/registers.c',
-                                 'pychronos/lux1310.c'])
+                      include_dirs = ['../lib'],
+                      depends = ['pychronos.h',
+                                 '../lib/fpga.h',
+                                 '../lib/lux1310.h'],
+                      sources = ['module.c',
+                                 'registers.c',
+                                 'lux1310.c'])
 
 setup (name = 'PyChronos',
        version = '0.3.1',
