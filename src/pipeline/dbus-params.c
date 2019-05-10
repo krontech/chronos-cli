3c4c39187b29f487a4446f1a8fc335e8e037e79e
Owen Kirby <oskirby@gmail.com>
2019-05-09 19:43:47 -0700

    Replace custom getters with a generic enum helper.
    The only two parameters that required a custom getter were videoState and
    focusPeakingColor to deal with enumerated int-to-string conversions.

--- src/pipeline/dbus-params.c
+++ src/pipeline/dbus-params.c
@@ -33,115 +33,77 @@ struct pipeline_param {
     GType       type;
     /* Simple parameters - just read it directly out of state using the type. */
     size_t      offset;
+    const void *extra;
     /* Complex parameters - callbacks to do translation and stateful stuff. */
-    void        (*getter)(struct pipeline_state *state, const struct pipeline_param *param, GHashTable *data);
     gboolean    (*setter)(struct pipeline_state *state, const struct pipeline_param *param, GValue *val);
 };
 
-struct enumval focus_peak_colors[] = {
-    {DISPLAY_CTL_FOCUS_PEAK_RED >> 6,       "red"},
-    {DISPLAY_CTL_FOCUS_PEAK_GREEN >> 6,     "green"},
-    {DISPLAY_CTL_FOCUS_PEAK_RED >> 6,       "blue"},
-    {DISPLAY_CTL_FOCUS_PEAK_CYAN >> 6,      "cyan"},
-    {DISPLAY_CTL_FOCUS_PEAK_MAGENTA >> 6,   "magenta"},
-    {DISPLAY_CTL_FOCUS_PEAK_YELLOW >> 6,    "yellow"},
-    {DISPLAY_CTL_FOCUS_PEAK_WHITE >> 6,     "white"},
+struct enumval playback_states[] = {
+    {PLAYBACK_STATE_PAUSE,      "paused"},
+    {PLAYBACK_STATE_LIVE,       "live"},
+    {PLAYBACK_STATE_PLAY,       "play"},
+    {PLAYBACK_STATE_FILESAVE,   "filesave"},
     { 0, NULL }
 };
 
-static void
-cam_video_state_getter(struct pipeline_state *state, const struct pipeline_param *param, GHashTable *data)
-{
-    switch (state->playstate) {
-        case PLAYBACK_STATE_PAUSE:
-        default:
-            cam_dbus_dict_add_string(data, param->name, "paused");
-            break;
-        case PLAYBACK_STATE_LIVE:
-            cam_dbus_dict_add_string(data, param->name, "live");
-            break;
-        case PLAYBACK_STATE_PLAY:
-            cam_dbus_dict_add_string(data, param->name, "playback");
-            break;
-        case PLAYBACK_STATE_FILESAVE:
-            cam_dbus_dict_add_string(data, param->name, "filesave");
-            break;
-    }
-}
+struct enumval focus_peak_colors[] = {
+    { 0,                            "black"},
+    {DISPLAY_CTL_FOCUS_PEAK_RED,    "red"},
+    {DISPLAY_CTL_FOCUS_PEAK_GREEN,  "green"},
+    {DISPLAY_CTL_FOCUS_PEAK_RED,    "blue"},
+    {DISPLAY_CTL_FOCUS_PEAK_CYAN,   "cyan"},
+    {DISPLAY_CTL_FOCUS_PEAK_MAGENTA, "magenta"},
+    {DISPLAY_CTL_FOCUS_PEAK_YELLOW, "yellow"},
+    {DISPLAY_CTL_FOCUS_PEAK_WHITE,  "white"},
+    { 0, NULL }
+};
 
-static void
-cam_focus_peak_color_getter(struct pipeline_state *state, const struct pipeline_param *param, GHashTable *data)
+static gboolean
+enumval_parse_gval(const struct enumval *e, GValue *gval, int *result)
 {
-    cam_dbus_dict_add_string(data, param->name, enumval_name(focus_peak_colors, state->config.peak_color >> 6, "disabled"));
+    /* If the value holds a string, search for a match. */
+    if (G_VALUE_HOLDS_STRING(gval)) {
+        const char *name;
+        for (name = g_value_get_string(gval); e->name; e++) {
+            if (strcasecmp(e->name, name) == 0) {
+                *result = e->value;
+                return TRUE;
+            }
+        }
+        return FALSE;
+    }
+    /* Although not recommended, also accept an integer. */
+    if (G_VALUE_HOLDS_UINT(gval)) {
+        *result = (int)g_value_get_uint(gval);
+    }
+    else if (G_VALUE_HOLDS_ULONG(gval)) {
+        *result = (int)g_value_get_ulong(gval);
+    }
+    else if (G_VALUE_HOLDS_INT(gval)) {
+        *result = g_value_get_int(gval);
+    }
+    else if (G_VALUE_HOLDS_LONG(gval)) {
+        *result = (int)g_value_get_long(gval);
+    }
+    else {
+        return FALSE;
+    }
+    /* Finally, ensure it's a valid enumerated value. */
+    while (e->name) {
+        if (e->value == *result) {
+            return TRUE;
+        }
+        e++;
+    }
+    return FALSE;
 }
 
 static gboolean
 cam_focus_peak_color_setter(struct pipeline_state *state, const struct pipeline_param *param, GValue *val)
 {
-    do {
-        /* If we got a boolean, then select Cyan as the default color. */
-        if (G_VALUE_HOLDS_BOOLEAN(val)) {
-            state->config.peak_color = g_value_get_boolean(val) ? DISPLAY_CTL_FOCUS_PEAK_CYAN : 0;
-            break;
-        }
-        /* Check for a string naming the color of choice. */
-        if (G_VALUE_HOLDS_STRING(val)) {
-            const char *color = g_value_get_string(val);
-            switch (tolower(color[0])) {
-                case 'r':
-                    state->config.peak_color = DISPLAY_CTL_FOCUS_PEAK_RED;
-                    break;
-                case 'g':
-                    state->config.peak_color = DISPLAY_CTL_FOCUS_PEAK_GREEN;
-                    break;
-                case 'b':
-                    state->config.peak_color = DISPLAY_CTL_FOCUS_PEAK_BLUE;
-                    break;
-                case 'c':
-                    state->config.peak_color = DISPLAY_CTL_FOCUS_PEAK_CYAN;
-                    break;
-                case 'm':
-                    state->config.peak_color = DISPLAY_CTL_FOCUS_PEAK_MAGENTA;
-                    break;
-                case 'y':
-                    state->config.peak_color = DISPLAY_CTL_FOCUS_PEAK_YELLOW;
-                    break;
-                case 'w':
-                    state->config.peak_color = DISPLAY_CTL_FOCUS_PEAK_WHITE;
-                    break;
-                default:
-                    state->config.peak_color = 0;
-                    break;
-            }
-            break;
-        }
-        /* Although not recommended, also accept an integer. */
-        if (G_VALUE_HOLDS_UINT(val)) {
-            state->config.peak_color = (g_value_get_uint(val) << 6) & DISPLAY_CTL_FOCUS_PEAK_COLOR;
-            break;
-        }
-        if (G_VALUE_HOLDS_ULONG(val)) {
-            state->config.peak_color = (g_value_get_ulong(val) << 6) & DISPLAY_CTL_FOCUS_PEAK_COLOR;
-            break;
-        }
-        if (G_VALUE_HOLDS_INT(val)) {
-            state->config.peak_color = (g_value_get_int(val) << 6) & DISPLAY_CTL_FOCUS_PEAK_COLOR;
-            break;
-        }
-        if (G_VALUE_HOLDS_LONG(val)) {
-            state->config.peak_color = (g_value_get_long(val) << 6) & DISPLAY_CTL_FOCUS_PEAK_COLOR;
-            break;
-        }
-        /* Otherwise, it's not a sane focus peaking color. */
-        return FALSE;
-    } while (0);
-
-    if (state->config.peak_color) {
-        state->control &= ~DISPLAY_CTL_FOCUS_PEAK_COLOR;
-        state->control |= (DISPLAY_CTL_FOCUS_PEAK_ENABLE | state->config.peak_color);
-    } else {
-        state->control &= ~DISPLAY_CTL_FOCUS_PEAK_ENABLE;
-    }
+    state->config.peak_color = g_value_get_int(val);
+    state->control &= ~DISPLAY_CTL_FOCUS_PEAK_COLOR;
+    state->control |= (state->config.peak_color);
 
     /* Update the FPGA if we're in live mode. */
     if (state->playstate == PLAYBACK_STATE_LIVE) {
@@ -161,10 +123,20 @@ cam_focus_peak_level_setter(struct pipeline_state *state, const struct pipeline_
     if (fplevel < 0.0) fplevel = 0.0;
     if (fplevel > 1.0) fplevel = 1.0;
     state->config.peak_level = fplevel;
+    if (state->config.peak_level > 0.0) {
+        state->control |= DISPLAY_CTL_FOCUS_PEAK_ENABLE;
+    } else {
+        state->control &= ~DISPLAY_CTL_FOCUS_PEAK_ENABLE;
+    }
 
     /* Update the FPGA directly if already in live mode. */
     if (state->playstate == PLAYBACK_STATE_LIVE) {
         state->fpga->display->peaking_thresh = 35 - (20 * fplevel);
+        if (state->config.peak_level > 0.0) {
+            state->fpga->display->control |= DISPLAY_CTL_FOCUS_PEAK_ENABLE;
+        } else {
+            state->fpga->display->control &= ~DISPLAY_CTL_FOCUS_PEAK_ENABLE;
+        }
     }
     return TRUE;
 }
@@ -214,16 +186,16 @@ cam_playback_length_setter(struct pipeline_state *state, const struct pipeline_p
 
 /* Table of parameters. */
 static const struct pipeline_param cam_dbus_params[] = {
-    { "videoState",         G_TYPE_STRING,  0, cam_video_state_getter, NULL},
+    { "videoState",         G_TYPE_ENUM,    offsetof(struct pipeline_state, playstate), playback_states, NULL},
     /* Exposure and focus aids. */
     { "overlayEnable",      G_TYPE_BOOLEAN, offsetof(struct pipeline_state, overlay.enable), NULL, cam_overlay_enable_setter},
     { "overlayFormat",      G_TYPE_STRING,  offsetof(struct pipeline_state, overlay.format), NULL, cam_overlay_format_setter},
-    { "focusPeakingColor",  G_TYPE_STRING,  0, cam_focus_peak_color_getter, cam_focus_peak_color_setter},
+    { "focusPeakingColor",  G_TYPE_ENUM,    offsetof(struct pipeline_state, config.peak_color), focus_peak_colors, cam_focus_peak_color_setter},
     { "focusPeakingLevel",  G_TYPE_DOUBLE,  offsetof(struct pipeline_state, config.peak_level), NULL, cam_focus_peak_level_setter},
     { "zebraLevel",         G_TYPE_DOUBLE,  offsetof(struct pipeline_state, config.zebra_level), NULL, NULL},
     /* Playback position and rate. */
-    { "playbackRate",       G_TYPE_LONG,    offsetof(struct pipeline_state, playrate), NULL, cam_playback_rate_setter},
-    { "playbackPosition",   G_TYPE_LONG,    offsetof(struct pipeline_state, position), NULL, cam_playback_position_setter},
+    { "playbackRate",       G_TYPE_LONG,    offsetof(struct pipeline_state, playrate),  NULL, cam_playback_rate_setter},
+    { "playbackPosition",   G_TYPE_LONG,    offsetof(struct pipeline_state, position),  NULL, cam_playback_position_setter},
     { "playbackStart",      G_TYPE_ULONG,   offsetof(struct pipeline_state, playstart), NULL, cam_playback_start_setter},
     { "playbackLength",     G_TYPE_ULONG,   offsetof(struct pipeline_state, playlength), NULL, cam_playback_length_setter},
     /* Quantity of recorded video. */
@@ -242,12 +214,6 @@ dbus_get_param(struct pipeline_state *state, const char *name, GHashTable *data)
         /* Look for a matching parameter by name. */
         if (strcasecmp(p->name, name) != 0) continue;
 
-        /* If a custom getter function exists - call it. */
-        if (p->getter) {
-            p->getter(state, p, data);
-            return TRUE;
-        }
-        
         /* Otherwise, parse the parameter out of the state structure. */
         pvalue = ((unsigned char *)state) + p->offset;
         switch (p->type) {
@@ -265,6 +231,9 @@ dbus_get_param(struct pipeline_state *state, const char *name, GHashTable *data)
                 return TRUE;
             case G_TYPE_STRING:
                 cam_dbus_dict_add_string(data, p->name, pvalue);
+                return TRUE;
+            case G_TYPE_ENUM:
+                cam_dbus_dict_add_string(data, p->name, enumval_name(p->extra, *(int *)pvalue, "undefined"));
                 return TRUE;
             default:
                 /* Unsupported type */
@@ -289,14 +258,21 @@ dbus_set_param(struct pipeline_state *state, const char *name, GValue *gval)
 
         /* Type transformation if necessary. */
         memset(&xform, 0, sizeof(xform));
-        g_value_init(&xform, p->type);
-
+        if (p->type == G_TYPE_ENUM) {
+            int result;
+            if (!enumval_parse_gval(p->extra, gval, &result)) return FALSE;
+            g_value_init(&xform, G_TYPE_INT);
+            g_value_set_int(&xform, result);
+            return p->setter(state, p, &xform);
+        }
         /* If the value holds the expected type - then set it. */
-        if (G_VALUE_HOLDS(gval, p->type)) {
+        else if (G_VALUE_HOLDS(gval, p->type)) {
             return p->setter(state, p, gval);
         }
+
         /* Otherwise, try our best to transform the type and set it. */
-        else if (g_value_transform(gval, &xform)) {
+        g_value_init(&xform, p->type);
+        if (g_value_transform(gval, &xform)) {
             return p->setter(state, p, &xform);
         }
         /* The user gave us a type we couldn't make sense of. */
