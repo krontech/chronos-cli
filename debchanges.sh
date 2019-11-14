52b3b102ec3fc4af222e33003ff52a83400f03b8
Owen Kirby <oskirby@gmail.com>
2019-11-14 14:08:20 -0800

    Fix debchanges.sh script for automated builds.
    For automated builds, the package builder is not likely to have a
    configured git username or e-mail address, in which case the auto
    changelog will be left with junk committer information. Instead we
    should parse these fields from the most-recent commit.

--- debchanges.sh
+++ debchanges.sh
@@ -1,15 +1,25 @@
-#!/bin/sh
+#!/bin/bash
+# Do all of these things from the git checkout.
+cd $(dirname ${BASH_SOURCE[0]})
+
+# Get the user name and e-mail from the last commit.
+DEBFULLNAME=$(git log -1 --format='%cn')
+DEBEMAIL=$(git log -1 --format='%ce')
+
+# Generate the Version Number from the most recent tag.
 TAGGED=$(git describe --tags --abbrev=0)
 REVISION=$(git rev-list ${TAGGED}..HEAD --count)
 VERSION=$(echo $TAGGED | sed -e 's/^[^0-9]*//g' | sed -e 's/-/~/g')
 if [ $REVISION -gt 0 ]; then
 	VERSION="${VERSION}${REVISION}"
 fi
+
+# Output the Debian changelog 
 cat << EOF
 chronos-cli (${VERSION}) unstable; urgency=medium
 
 $(git log --oneline ${TAGGED}..HEAD | sed -e 's/^[0-9a-f]*/  \*/g')
   * Upstream release ${TAGGED}
 
- -- $(git config user.name) <$(git config user.email)>  $(date -R)
+ -- ${DEBFULLNAME} <${DEBEMAIL}>  $(date -R)
 EOF
